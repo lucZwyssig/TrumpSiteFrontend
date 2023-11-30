81ca1ad53ed2813c27d526e2ee876ec1fb99358f
luc zwyssig <luc.zwyssig@stud.kbw.ch>
2023-11-30 12:30:43 +0100

    feat: changed index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home";
+import Timeline from "./Pages/Timeline";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Article from "./Pages/Article";
+ 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/timeline",
+    element: <Timeline />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/article/:Id",
+    element: <Article />,
+  },
+]);
+ 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 reportWebVitals();
+ 
